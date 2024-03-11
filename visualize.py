5fd6e417ead51ff4b6c2663ba0c881222230336f
mahboobelahi93 <mahboobelahi93@gmail.com>
2024-03-11 14:54:42 +0200

    removed unused plotting functions

--- visualize.py
+++ visualize.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-label = 0
+
 pallete = ['darkgreen', 'tomato', 'yellow', 'darkblue', 'darkviolet', 'indianred', 'yellowgreen', 'mediumblue', 'cyan',
            'black', 'indigo', 'pink', 'lime', 'sienna', 'plum', 'deepskyblue', 'forestgreen', 'fuchsia', 'brown',
            'turquoise', 'aliceblue', 'blueviolet', 'rosybrown', 'powderblue', 'lightblue', 'skyblue', 'lightskyblue',
@@ -16,80 +16,6 @@ color_pallete = ['lightsalmon', 'lightseagreen', 'lavenderblush', 'aquamarine', 
                  'darkslategrey', 'teal', 'darkcyan', 'aqua', 'cyan', 'darkturquoise', 'cadetblue', 'thistle',
                  'violet', 'purple', 'darkmagenta', 'magenta', 'orchid', 'mediumvioletred', 'deeppink', 'hotpink',
                  'palevioletred', 'crimson', 'lightpink']
-
-
-def cuboid_data(o, size=(1, 1, 1)):
-    # suppose axis direction: x: to left; y: to inside; z: to upper
-    # get the length, width, and height
-    l, w, h = size
-    x = [[o[0], o[0] + l, o[0] + l, o[0], o[0]],
-         [o[0], o[0] + l, o[0] + l, o[0], o[0]],
-         [o[0], o[0] + l, o[0] + l, o[0], o[0]],
-         [o[0], o[0] + l, o[0] + l, o[0], o[0]]]
-    y = [[o[1], o[1], o[1] + w, o[1] + w, o[1]],
-         [o[1], o[1], o[1] + w, o[1] + w, o[1]],
-         [o[1], o[1], o[1], o[1], o[1]],
-         [o[1] + w, o[1] + w, o[1] + w, o[1] + w, o[1] + w]]
-    z = [[o[2], o[2], o[2], o[2], o[2]],
-         [o[2] + h, o[2] + h, o[2] + h, o[2] + h, o[2] + h],
-         [o[2], o[2], o[2] + h, o[2] + h, o[2]],
-         [o[2], o[2], o[2] + h, o[2] + h, o[2]]]
-    return np.array(x), np.array(y), np.array(z)
-
-
-def plotcuboid(pos=(0, 0, 0), size=(1, 1, 1), ax=None, lab=label,**kwargs):
-    # Plotting a cube element at position pos
-    if ax is not None:
-        x,y,z =pos
-        length ,width, height =size
-        X, Y, Z = cuboid_data(pos, size)
-        
-        ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
-                         edgecolor='black', alpha=0.3,linewidth=1.2, edgecolors='r', **kwargs)
-
-        for dx, dy, dz in [(length/2 ,width/2 , height),
-                           (length ,width/2 , height/2),(0 ,width/2 , height/2),
-                            (length/2 ,0 , height/2),(length/2 ,width , height/2),
-                            (length/2 ,width/2 , height*(-.5))]:
-            ax.text(x + dx, y + dy, z + dz, str(label),alpha=0, color='black', fontsize=15, ha='center', va='center')
-
-def draw( x_ticks, y_ticks, z_ticks, step_size,pieces, color_index=[], title=""):
-    global label
-    positions = []
-    sizes = []
-    colors = []
-    sorted_size = []
-    decrement =30
-    for each in pieces:
-        positions.append(each[0:3])
-        sizes.append(each[3:])
-        sorted_size.append(set(each[3:]))
-    if len(color_index) == 0:
-        colors = pallete[:len(positions)]
-        color_index = [sorted_size, colors]
-    else:
-        dim = color_index[0]
-        clr = color_index[1]
-        for each in sorted_size:
-            index = dim.index(each)
-            colors.append(clr[index])
-    plt.interactive(True)
-    fig = plt.figure(figsize=(15,20))
-
-    ax = fig.add_subplot(projection='3d')
-    ax.set_xticks(np.arange(0, x_ticks + step_size-decriment, step_size-decriment))
-    ax.set_yticks(np.arange(0, y_ticks + step_size-40, step_size-40))
-    ax.set_zticks(np.arange(0, z_ticks + step_size-40, step_size-40))
-    
-    for p, s, c in zip(positions, sizes, colors):
-        label = label+1
-        plotcuboid(pos=p, size=s, ax=ax, color=c,lab=label)
-    label =0
-    plt.title(title)
-
-    plt.show()
-
-    return color_index
 
 from mpl_toolkits.mplot3d import Axes3D
 decriment=30
